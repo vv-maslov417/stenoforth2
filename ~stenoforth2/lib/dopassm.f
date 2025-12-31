2123a66a363a1c589c2850cc48d86bd46c712cbe
vv-maslov417 <v.maslov2552@mail.ru>
2025-12-31 21:41:27 +0300

    Add files via upload

--- ~stenoforth2/lib/dopassm.f
+++ ~stenoforth2/lib/dopassm.f
@@ -2,19 +2,21 @@
 
 \ Simplify the recording of immediate values
 \ $ 4 --> 4h $ 20 --> 32d  $ A  --> 1010b
-rec: nmb?[ a u 1- number? ]
-     smb?[ a u + 1- c@ = ]
-     snmb?[ smb? nmb? nip nip and ]
-     'h' snmb? 'd' snmb? or 'b' snmb? or
-gen: 'b' smb? if 0x2 BASE ! nmb? then
-     'd' smb? if nmb? then
-     'h' smb? if hex nmb? then
-     2drop >CS decimal ;
+rec: nmb[ a u 1- number? 3\31 ]
+     a u + 1- c@ smb\
+          smb 'd' = if decimal    nmb
+     else smb 'h' = if hex        nmb
+     else smb 'b' = if 0x2 base ! nmb
+     else 0 0 then then then  NMB\
+gen: NMB >CS decimal ;
 
 \ saving registers in memory
 m: A>a [ a) a >CS ] @=A ; m: B>b [ b) b >CS ] @=B ;
 m: C>c [ c) c >CS ] @=C ; m: D>d [ d) d >CS ] @=D ;
-m: S>s [ e) e >CS ] @=S ; m: T>t [ f) f >CS ] @=S ;
+m: S>s [ s) s >CS ] @=S ; m: T>t [ t) t >CS ] @=T ;
+m: A>a1 [ a1) a1 >CS ] @=A ; m: B>b1 [ b1) b1 >CS ] @=B ;
+m: C>c1 [ c1) c1 >CS ] @=C ; m: D>d1 [ d1) d1 >CS ] @=D ;
+m: S>s1 [ s1) s1 >CS ] @=S ; m: T>t1 [ t1) t1 >CS ] @=T ;
 
 \ restoring registers from memory
 m: a>A [ a >CS ] A=@ ; m: a>B [ a >CS ] B=@ ; m: a>C [ a >CS ] C=@ ;
@@ -29,3 +31,5 @@ m: s>A [ s >CS ] A=@ ; m: s>B [ s >CS ] B=@ ; m: s>C [ s >CS ] C=@ ;
 m: s>D [ s >CS ] D=@ ; m: s>S [ s >CS ] S=@ ; m: s>T [ s >CS ] T=@ ;
 m: t>A [ t >CS ] A=@ ; m: t>B [ t >CS ] B=@ ; m: t>C [ t >CS ] C=@ ;
 m: t>D [ t >CS ] D=@ ; m: t>S [ t >CS ] S=@ ; m: t>T [ t >CS ] T=@ ;
+m: a1>A [ a1 >CS ] A=@ ; m: b1>B [ b1 >CS ] B=@ ; m: c1>C [ c1 >CS ] C=@ ;
+m: d1>D [ d1 >CS ] D=@ ; m: s1>S [ s1 >CS ] S=@ ; m: t1>T [ t1 >CS ] T=@ ;
